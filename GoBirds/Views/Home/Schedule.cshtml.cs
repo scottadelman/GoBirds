1210342ce20498b3472b57ea391f51e9b4a35ca2
Scott Adelman <scottadelman97@gmail.com>
2024-05-28 20:42:33 -0400

    Added countdown timers for pre/reg season

---
+++ GoBirds/Views/Home/Schedule.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GoBirds.Views.Home
+{
+    public class ScheduleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
